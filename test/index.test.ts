f688a292a60c5f9e6ba48bdb8ec5e12691d67a17
Doma <leishenghao@126.com>
2023-02-13 18:30:38 +0800

    feat: prettier-plugin-sort-markdown-table

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,35 @@
 import { test, assert } from "vitest"
-import { foo } from "../src"
+import prettier from "prettier"
+import plugin from "../src"
 
-test("simple", () => {
-  assert.equal(foo, "foo")
+test("Sort markdown table following prettier-sort-markdown-table annotation", () => {
+  const markdown = `
+<!-- prettier-sort-markdown-table -->
+| name | email | description |
+| -- | -- | --- |
+| zhang | zhang@a.com  | zhang  |
+| wang | wang@a.com   | wang |
+| li | li@a.com   | li  |
+| amity | amity@a.com  | amity  |
+| batman | batman@a.com | batman |
+`
+
+  const formatted = `<!-- prettier-sort-markdown-table -->
+
+| name   | email        | description |
+| ------ | ------------ | ----------- |
+| amity  | amity@a.com  | amity       |
+| batman | batman@a.com | batman      |
+| li     | li@a.com     | li          |
+| wang   | wang@a.com   | wang        |
+| zhang  | zhang@a.com  | zhang       |
+`
+
+  assert.equal(
+    prettier.format(markdown, {
+      parser: "markdown",
+      plugins: [plugin],
+    }),
+    formatted,
+  )
 })
