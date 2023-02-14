b628a5f201fcded4b1ec3552c0020c18abd98f6e
Doma <leishenghao@126.com>
2023-02-14 14:49:07 +0800

    fix: fix exports not recognized by prettier

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,47 @@
 import type { Plugin } from "prettier"
-import { parsers } from "prettier/parser-markdown"
+import { parsers as prettierMarkdownParsers } from "prettier/parser-markdown"
 
-const markdownParser = parsers["markdown"]
+const markdownParser = prettierMarkdownParsers["markdown"]
 
-const plugin: Plugin = {
-  parsers: {
-    markdown: {
-      ...markdownParser,
-      preprocess(text, options) {
-        if (markdownParser.preprocess) {
-          text = markdownParser.preprocess(text, options)
-        }
+export const parsers: Plugin["parsers"] = {
+  markdown: {
+    ...markdownParser,
+    preprocess(text, options) {
+      if (markdownParser.preprocess) {
+        text = markdownParser.preprocess(text, options)
+      }
 
-        // Sort the table directly following <!-- prettier-sort-markdown-table --> annotation (if any)
-        text = text.replace(
-          // Regex reference: https://stackoverflow.com/a/29616512
-          /<!--\s*prettier-sort-markdown-table\s*-->\r?\n(?:\s*\r?\n)*(\|(?:([^\r\n|]*)\|)+\r?\n\|(?:(\s*:?-+:?\s*)\|)+\r?\n(\|(?:([^\r\n|]*)\|)+(?:\r?\n)?)+)/g,
-          (match) => {
-            const lines = match.split(/\r?\n/)
+      // Sort the table directly following <!-- prettier-sort-markdown-table --> annotation (if any)
+      text = text.replace(
+        // Regex reference: https://stackoverflow.com/a/29616512
+        /<!--\s*prettier-sort-markdown-table\s*-->\r?\n(?:\s*\r?\n)*(\|(?:([^\r\n|]*)\|)+\r?\n\|(?:(\s*:?-+:?\s*)\|)+\r?\n(\|(?:([^\r\n|]*)\|)+(?:\r?\n)?)+)/g,
+        (match) => {
+          const lines = match.split(/\r?\n/)
 
-            const annotationLines = [lines.shift()]
+          const annotationLines = [lines.shift()]
 
-            // Push empty lines between annotation and table
-            while (!lines[0].startsWith("|")) {
-              annotationLines.push(lines.shift())
-            }
+          // Push empty lines between annotation and table
+          while (!lines[0].startsWith("|")) {
+            annotationLines.push(lines.shift())
+          }
 
-            const tableHeaderLines = [lines.shift(), lines.shift()]
+          const tableHeaderLines = [lines.shift(), lines.shift()]
 
-            // Sort table body rows here
-            const tableBodyLines = lines.filter(Boolean).sort()
+          // Sort table body rows here
+          const tableBodyLines = lines.filter(Boolean).sort()
 
-            // The regex matched trailing newlines following the table
-            const trailingNewlines = lines.slice(tableBodyLines.length)
+          // The regex matched trailing newlines following the table
+          const trailingNewlines = lines.slice(tableBodyLines.length)
 
-            return [
-              ...annotationLines,
-              ...tableHeaderLines,
-              ...tableBodyLines,
-              ...trailingNewlines,
-            ].join("\n")
-          },
-        )
-        return text
-      },
+          return [
+            ...annotationLines,
+            ...tableHeaderLines,
+            ...tableBodyLines,
+            ...trailingNewlines,
+          ].join("\n")
+        },
+      )
+      return text
     },
   },
 }
-
-export default plugin
